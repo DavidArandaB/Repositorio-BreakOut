ccab375c3f96d156f7e8b171cd28a937f7762862
David Aranda Baltazar <adavid.abaltazar@gmail.com>
2025-10-04 23:05:53 -0600

    Tarea completada
    Se crean los cambios para la tarea 12

--- BreakOut/Assets/_Scripts/Player.cs
+++ BreakOut/Assets/_Scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] public int limitex = 23;
+    [SerializeField] public float paddleSpeed = 10.0f;
+    Vector3 MousePos2D;
+    Vector3 MousePos3D;
+    Transform transformGO;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transformGO = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //MousePos2D = Input.mousePosition;
+        //MousePos2D.z = -Camera.main.transform.position.z;
+        //MousePos3D = Camera.main.ScreenToWorldPoint(MousePos2D);
+
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    this.transform.Translate(Vector3.down * paddleSpeed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    this.transform.Translate(Vector3.up * paddleSpeed * Time.deltaTime);
+        //}
+
+        this.transform.Translate(Input.GetAxis("Horizontal")*Vector3.down * paddleSpeed * Time.deltaTime);
+
+        Vector3 pos = transform.position;
+        //transform.position.x = MousePos3D.x;
+        if (pos.x > limitex) {
+            pos.x = limitex;
+        }
+        else if (pos.x < -limitex)
+        {
+            pos.x = -limitex;
+        }
+        transformGO.position = pos;
     }
 }
